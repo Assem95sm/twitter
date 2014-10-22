b9383987e57d26181f9149c3eac4e41a8c694f4d
Muzhtabina Assem <asem95sm@gmail.com>
2014-10-22 13:55:54 +0000

    add static pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  
+  def full_title(page_title)
+    if page_title.empty?
+      return "Twitter"
+    else
+      return"#{page_title} / Twiiter"
+    end
+
+  end
+  
 end
